52fcb41b388f54d89042d51a48a58197a8edcd5a
alimohamedgad <aligad2580@gmail.com>
2023-09-16 02:42:55 +0300

    some refactor

--- lib/core/app_image/image.dart
+++ lib/core/app_image/image.dart
@@ -3,6 +3,6 @@ class AppImage {
   static const pizza = 'assets/images/food/realPizza.png';
   static const crepe = 'assets/images/food/realCrepe.png';
   static const shawrma = 'assets/images/food/shawrma2.png';
-  static const splashImage = 'assets/images/virtual/splashIcon.png';
+  static const splashImage = 'assets/images/food/splashIcon.png';
   static const emptyCart = 'assets/images/food/empty.png';
 }
